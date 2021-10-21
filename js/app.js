aa3f83b87bcc4ce05ff024efb44d8c41f9797d55
chadonet-67 <chadonettzt@gmail.com>
2021-10-21 15:46:34 +0200

    creation du projet

---
+++ js/app.js
@@ -0,0 +1,65 @@
+let searchForm = document.querySelector(".search-form");
+let shoppingCart = document.querySelector(".shopping-cart");
+let loginForm = document.querySelector(".login-form");
+let menuBtn = document.querySelector('.navbar')
+document.querySelector("#search-btn").onclick = () =>{
+    searchForm.classList.toggle("active");
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+   menuBtn.classList.remove('active');
+
+}
+
+document.querySelector("#cart-btn").onclick = () =>{
+    shoppingCart.classList.toggle("active");
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+   menuBtn.classList.remove('active');
+
+}
+
+document.querySelector("#login-btn").onclick = () =>{
+    loginForm.classList.toggle("active");
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+   menuBtn.classList.remove('active');
+
+}
+
+document.querySelector("#menu-btn").onclick = () =>{
+    menuBtn.classList.toggle("active");
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+
+}
+
+window.onscroll = ()=>{
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+   menuBtn.classList.remove('active');
+
+}
+
+
+var swiper = new Swiper(".product-slider", {
+    loop:true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    autoplay:{
+       delay:7500,
+       disableOnInteraction:false,
+   },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+  });
